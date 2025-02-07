ec6ba6d8e12f392d44e4f122f80cc69923c7a723
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-02-07 14:49:20 -0600

    Initial Commit

---
+++ my-clock/src/components/Clock.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+let time = ref("");
+let timeInterval = null;
+
+function updateTime(format) {
+  const currentTime = new Date();
+  // const testTime = new Date("2022-01-01 0:00:00")
+  let hours = currentTime.getHours();
+  // let hours = 0
+  const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+  const seconds = String(currentTime.getSeconds()).padStart(2, "0");
+  let AM_PM = "";
+  // console.log(format)
+  if (format === "12-hour") {
+    if (hours >= 12 || hours === 0) {
+      AM_PM = " PM";
+      if (hours === 0) {
+        hours = 12;
+      }
+      if (hours >= 13) {
+        hours -= 12;
+      }
+    } else {
+      AM_PM = " AM";
+    }
+  }
+  time.value = `${hours}:${minutes}:${seconds}${AM_PM}`;
+}
+
+let format = "12-hour";
+
+function switchFormat() {
+  switch (format) {
+    case "12-hour":
+      format = "24-hour";
+      break;
+    case "24-hour":
+      format = "12-hour";
+      break;
+  }
+  updateTime(format);
+}
+
+onMounted(() => {
+  updateTime(format);
+  timeInterval = setInterval(() => updateTime(format), 1000);
+});
+onUnmounted(() => {
+  clearInterval(timeInterval);
+  timeInterval = null;
+});
+</script>
+
+<template>
+  <div class="clock">{{ time }}</div>
+  <div><button @click="switchFormat">12hr/24hr</button></div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from "node:url";
+import { dirname, resolve } from "node:path";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools({ launchEditor: "webstorm" })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        exampleProject: resolve(__dirname, "./example-project/app.html"),
+        myClock: resolve(__dirname, "./my-clock/app.html"),
+      },
+    },
+  },
+});
